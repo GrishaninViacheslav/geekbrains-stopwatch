88282ab2e6c7e434a7ad53580597aacc6fd27a94
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-11-11 19:32:45 +0300

    Перенос кода из методички

---
+++ app/src/main/java/io/github/grishaninvyacheslav/geekbrains_stopwatch/StopwatchState.kt
@@ -0,0 +1,13 @@
+package io.github.grishaninvyacheslav.geekbrains_stopwatch
+
+sealed class StopwatchState {
+
+    data class Paused(
+        val elapsedTime: Long
+    ) : StopwatchState()
+
+    data class Running(
+        val startTime: Long,
+        val elapsedTime: Long
+    ) : StopwatchState()
+}
---
+++ app/src/main/java/io/github/grishaninvyacheslav/geekbrains_stopwatch/TimestampProvider.kt
@@ -0,0 +1,5 @@
+package io.github.grishaninvyacheslav.geekbrains_stopwatch
+
+interface TimestampProvider {
+    fun getMilliseconds(): Long
+}
